282fea0deb238cc49b7da2ef33add72a191ae850
Tilo Prütz <tilo@infopark.de>
2017-06-30 11:44:41 +0200

    get Infopark account IDs from ENV or config file

--- lib/infopark/aws_utils.rb
+++ lib/infopark/aws_utils.rb
@@ -1,10 +1,9 @@
 require 'infopark/aws_utils/env'
+require 'json'
 
 module Infopark
   module AwsUtils
     AWS_AMI_OWNER = "137112412989"
-    DEV_ACCOUNT_ID = "012615398682"
-    PROD_ACCOUNT_ID = "115379056088"
 
     class << self
       def gather_all(client, method, **options)
@@ -34,6 +33,25 @@ module Infopark
           w.before_wait { progress.increment }
         end
       end
+
+      protected
+
+      def local_config
+        @local_config ||= (
+          path = "#{Dir.home}/.config/infopark/aws_utils.json"
+          if File.exists?(path)
+            JSON.parse(File.read(path))
+          else
+            {}
+          end
+        )
+      end
     end
+
+    DEV_ACCOUNT_ID = ENV['INFOPARK_AWS_DEV_ACCOUNT_ID'] || self.local_config['dev_account_id']
+    PROD_ACCOUNT_ID = ENV['INFOPARK_AWS_PROD_ACCOUNT_ID'] || self.local_config['prod_account_id']
+
+    puts "WARN: The Infopark AWS development account ID is not configured." unless DEV_ACCOUNT_ID
+    puts "WARN: The Infopark AWS production account ID is not configured." unless PROD_ACCOUNT_ID
   end
 end
--- lib/infopark/aws_utils/version.rb
+++ lib/infopark/aws_utils/version.rb
@@ -1,5 +1,5 @@
 module Infopark
   module AwsUtils
-    VERSION = "0.0.2"
+    VERSION = "0.1.0"
   end
 end
